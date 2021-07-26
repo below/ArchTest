db3030cf645730e70d994d345350dc43e0f66568
Smarthome Buildprozess <fmbsmarthome_buildprozess@telekom.de>
2021-07-26 18:28:33 +0200

    Initial Commit

---
+++ ArchTest/ArchTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchTestApp.swift
+//  ArchTest
+//
+//  Created by SmartHome CICD on 26.07.21.
+//
+
+import SwiftUI
+
+@main
+struct ArchTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArchTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ArchTest
+//
+//  Created by SmartHome CICD on 26.07.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
